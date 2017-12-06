73f0d1e392bd3b2ede62c90aa6c045895b25a152
xxxgreen <mvxgreen@gmail.com>
2017-12-06 13:56:22 -0600

    main.go prints cmd-line args

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
   "fmt"
+  "os"
 )
 
 func main() {
-  fmt.Println("What's up, world?")
+  var s, sep string
+  for i := 1; i < len(os.Args); i++ {
+    s += sep + os.Args[i]
+    sep = " "
+  }
+  fmt.Println(s)
 }
