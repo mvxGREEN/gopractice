44908cc0fe90cb8fb94b846575218742ecbf9a37
xxxgreen <mvxgreen@gmail.com>
2017-12-07 02:39:28 -0600

    holy shit, it's working!

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
   http.HandleFunc("/", handler)
-  log.Fatal(http.ListenAndServe("localhost:8000", nil))
+  log.Fatal(http.ListenAndServe("192.168.1.16:80", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
