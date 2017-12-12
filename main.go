ddc28386368d66ae0f996c2df5e7a201691027c8
xxxgreen <mvxgreen@gmail.com>
2017-12-12 12:42:57 -0600

    added background image, made header transparent w/ blur

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ func main() {
 	e.Static("/", "public")
 	e.GET("/", Index)
   e.GET("/wow", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Goodbye, World!")
 	})
 
 
